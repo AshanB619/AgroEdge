ca2bfa8843d9c0d2ee584b5b85af65a8b42c79e5
Anjana Ranasinghe <anjana.20231160@iit.ac.lk>
2024-12-29 14:26:47 +0530

    Updated App.jsx & index.html files

---
+++ Community Page/client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react'; 
+import Cookies from 'universal-cookie'; //import three dependencies 
+
+const apiKey = 'zeg2jgbzuu7g';
+
+const App = () =>{
+    return(
+        <div>
+            <h1>Community page Chat</h1>
+        </div>
+    )
+}
+
+export default App
