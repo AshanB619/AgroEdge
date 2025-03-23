7226ac3b41c0d2984df8b3b02844ef2ab5602f73
Anjana Ranasinghe <anjana.20231160@iit.ac.lk>
2025-03-23 22:20:15 +0530

    Test2

--- CommunityChat2/client_side/jest.config.js
+++ CommunityChat2/client_side/jest.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-    testEnvironment: 'jsdom', // For React/Next.js testing
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1', // Handle Next.js alias for imports (e.g., @/components)
-    },
-  };
+// module.exports = {
+//     testEnvironment: 'jsdom', // For React/Next.js testing
+//     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//     moduleNameMapper: {
+//       '^@/(.*)$': '<rootDir>/$1', // Handle Next.js alias for imports (e.g., @/components)
+//     },
+//   };
